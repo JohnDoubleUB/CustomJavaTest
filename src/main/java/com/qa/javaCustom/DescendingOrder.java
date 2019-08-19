f388a0c286d5ca0d92289167cf1f7310e7ed3396
John <john.brockman@academytrainee.com>
2019-08-19 15:21:15 +0100

    Added Descending Order Challenge and JUnit Test File!

---
+++ src/main/java/com/qa/javaCustom/DescendingOrder.java
@@ -0,0 +1,13 @@
+package com.qa.javaCustom;
+
+public class DescendingOrder {
+	
+	//sortDesc(123) -> 321
+	//sortDesc(1254859723) -> 9875543221
+	//sortDesc(73065) -> 76530
+	
+	
+	public static int sortDesc(int number) {
+		return 0;
+	}
+}
---
+++ src/test/java/com/qa/javaCustomTest/DescendingOrderTest.java
@@ -0,0 +1,59 @@
+package com.qa.javaCustomTest;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import com.qa.javaCustom.DescendingOrder;
+
+public class DescendingOrderTest {
+
+    @Test
+    public void test_01() {
+        assertEquals(321, DescendingOrder.sortDesc(123));
+    }
+
+    @Test
+    public void test_02() {
+        assertEquals(977766200, DescendingOrder.sortDesc(670276097));
+    }
+
+    @Test
+    public void test_03() {
+        assertEquals(9865210, DescendingOrder.sortDesc(2619805));
+    }
+
+    @Test
+    public void test_04() {
+        assertEquals(98421, DescendingOrder.sortDesc(81294));
+    }
+
+    @Test
+    public void test_05() {
+        assertEquals(987654321, DescendingOrder.sortDesc(123495678));
+    }
+
+		@Test
+    public void test_06() {
+        assertEquals(98421, DescendingOrder.sortDesc(81294));
+    }
+
+    @Test
+    public void test_07() {
+        assertEquals(0000000, DescendingOrder.sortDesc(0000000));
+    }
+
+		@Test
+    public void test_08() {
+        assertEquals(321, DescendingOrder.sortDesc(321));
+    }
+
+		@Test
+    public void test_09() {
+        assertEquals(986420, DescendingOrder.sortDesc(628904));
+    }
+
+		@Test
+    public void test_10() {
+        assertEquals(987653220, DescendingOrder.sortDesc(289327560));
+    }
+}
